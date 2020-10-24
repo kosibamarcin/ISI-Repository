458ff42cce076bcb6a92b77aa52cf0a0f561a7dd
kosibamarcin <71263035+kosibamarcin@users.noreply.github.com>
2020-10-24 14:25:51 +0200

    Create main.c

---
+++ nowy/cmake-build-debug/CMakeFiles/nowy.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nowy.dir/main.c.obj"
+  "libnowy.dll.a"
+  "nowy.exe"
+  "nowy.exe.manifest"
+  "nowy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/nowy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
